753735c92b36b15bf7bbe86096f799103ff8e8eb
Alejandro <ftvalex@hotmail.com>
2024-06-03 18:42:52 +0200

    ca

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 // src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as csurf from 'csurf';
-import helmet from "helmet";
+
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,  { cors: true });
 
-  app.use(helmet());
-  app.use(csurf());
 
   await app.listen(3000);
 }
