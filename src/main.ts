f7c633fef4c3942a4c83951177dfa94b8aaa1657
Alejandro <ftvalex@hotmail.com>
2024-06-03 19:16:06 +0200

    ajustes

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,  { cors: true });
-
-
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
