b1ca8c308362d1b357c61376f5c6811011b86330
Alejandro <ftvalex@hotmail.com>
2024-06-04 17:21:41 +0200

    volviendo al login mio

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,39 +1,39 @@
+
 // src/auth/auth.service.ts
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from '../users/users.service';
 import { LoginUserDto } from './dto/login-user.dto';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
-import '../firebase.config'; 
+import { UserDocument } from '../users/schema/user.schema';
 
 @Injectable()
 export class AuthService {
   constructor(
-    //private usersService: UsersService, ahora usamos Firebase
+    private usersService: UsersService,
     private jwtService: JwtService
   ) {}
 
   async validateUser(username: string, pass: string): Promise<any> {
-    const auth = getAuth();
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, username, pass);
-      const user = userCredential.user;
-      return { username: user.email, uid: user.uid };
-    } catch (error) {
+    const user = await this.usersService.validateUser(username, pass);
+    if (!user) {
       return null;
     }
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
   }
 
-  async login(loginUserDto: LoginUserDto): Promise<any> {
-    const user = await this.validateUser(loginUserDto.username, loginUserDto.password);
-    if (!user) {
-      throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
-    }
-    const payload = { username: user.username, sub: user.uid };
-    return {
-      access_token: this.jwtService.sign(payload),
-      username: user.username,
-      userId: user.uid
-    };
+// AuthService en NestJS
+async login(loginUserDto: LoginUserDto): Promise<any> {
+  const user = await this.usersService.validateUser(loginUserDto.username, loginUserDto.password);
+  if (!user) {
+    throw new HttpException('Invalid credentials', HttpStatus.UNAUTHORIZED);
   }
+  const payload = { username: user.username, sub: user._id };
+  return {
+    access_token: this.jwtService.sign(payload),
+    username: user.username,
+    userId: user._id  
+  };
+}
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import './firebase.config'; 
+
 require('dotenv').config();
 
 async function bootstrap() {
