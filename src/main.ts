0f42e3475bdb35c394942b789202f798c5c32df0
Alejandro <ftvalex@hotmail.com>
2024-06-06 17:22:00 +0200

    CORS

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { AppModule } from './app.module';
 require('dotenv').config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(process.env.PORT || 3000);
+  const app = await NestFactory.create(AppModule);
+  
+  const allowedOrigins = process.env.CORS_ORIGINS?.split(',') || [];
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
